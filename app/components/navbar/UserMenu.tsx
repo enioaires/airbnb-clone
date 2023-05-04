3663db9436e58e2176f9330c0cdd049193582c5e
Enio <enio.filho@deal.com.br>
2023-05-04 20:50:29 -0300

    first commit

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { Menu } from 'lucide-react'
+import { FC, useCallback, useState } from 'react'
+import Avatar from '../Avatar'
+import MenuItem from './MenuItem'
+
+interface UserMenuProps {
+
+}
+
+const UserMenu: FC<UserMenuProps> = ({ }) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((prev) => !prev)
+    }, [])
+    return (
+        <div className='relative'>
+            <div className='flex items-center gap-3'>
+                <div
+                    onClick={() => { }}
+                    className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
+                >
+                    Airbnb sua casa
+                </div>
+                <div
+                    onClick={toggleOpen}
+                    className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
+                >
+                    <Menu />
+                    <div className='hidden md:block'>
+                        <Avatar />
+                    </div>
+                </div>
+            </div>
+
+            {isOpen && (
+                <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+                    <div className='flex cursor-pointer flex-col'>
+                        <>
+                            <MenuItem
+                                onClick={() => { }}
+                                label='Cadastrar-se'
+                            />
+                            <MenuItem
+                                onClick={() => { }}
+                                label='Entrar'
+                            />
+                        </>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default UserMenu
